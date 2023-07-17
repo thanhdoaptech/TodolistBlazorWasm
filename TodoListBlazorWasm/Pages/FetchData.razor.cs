3215c48b28f941670170b4a75a5de60f99a7221d
thanhdoaptech <thanhdoaptech@gmail.com>
2023-07-18 00:19:20 +0900

    update 1

---
+++ TodoListBlazorWasm/Pages/FetchData.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace TodoListBlazorWasm.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[]? forecasts;
+        [Inject] HttpClient Http { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+
+        public class WeatherForecast
+        {
+            public DateOnly Date { get; set; }
+
+            public int TemperatureC { get; set; }
+
+            public string? Summary { get; set; }
+
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
